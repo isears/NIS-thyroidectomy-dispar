b83affea9397f1f93b31179651d65751d2eb0c12
isears <isaac.j.sears@gmail.com>
2022-08-02 15:33:52 -0400

    Refactor codes to dataprocessing root

---
+++ src/nistd/dataProcessing/__init__.py
@@ -0,0 +1,12 @@
+# TODO
+thyroidectomy_icd10_codes = [
+    "0GBG0ZX",
+    "0GBG0ZZ",
+    "0GBG3ZX",
+    "0GBG3ZZ",
+    "0GBG4ZX",
+    "0GBG4ZZ",
+]
+thyroidectomy_icd9_codes = ["062", "063", "0631", "0639", "064"]
+
+thyroidectomy_codes = thyroidectomy_icd9_codes + thyroidectomy_icd10_codes
